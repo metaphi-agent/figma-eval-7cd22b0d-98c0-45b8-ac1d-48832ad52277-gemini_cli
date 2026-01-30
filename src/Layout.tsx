dcf539524d9fabbce54ea757917eae4dd409214f
chandwaniac <149376785+chandwaniac@users.noreply.github.com>
2026-01-31 04:07:09 +0530

    Fix import paths in Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Header } from '../components/blocks/Header';
-import { Footer } from '../components/blocks/Footer';
+import { Header } from './components/blocks/Header';
+import { Footer } from './components/blocks/Footer';
 
 interface LayoutProps {
   children: React.ReactNode;
